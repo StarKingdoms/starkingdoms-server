eab3338dc5fc5d84199f0e75765af125b32232b6
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-29 13:05:50 -0400

    Create online.php

---
+++ api/v1/online.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * StarKingdoms API
+ * -=- metadata -=-
+ * 
+ * filename=online.php
+ * version=1
+ * endpoint=/online
+ * fullurl=/api/v1/online
+ * response:text/json
+ * apilevel=public-nokeyed
+ * keyfile=none
+ */
+
+echo '{"online": true, "version": 1}';
+
+?>
