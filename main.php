a6cdf97f14163399b5a3daab201260ac37297430
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-06-25 09:05:33 -0400

    Update main.php

--- main.php
+++ main.php
@@ -27,5 +27,17 @@
 	</div>
   <canvas id="canvas"></canvas>
   <script src="static/scripts/client.js?t=<?php echo mktime();?>"></script>
+  <script>
+  function initFingerprintJS() {
+    FingerprintJS.load({token: 'oLzKhv5gzqsrHIhqWv2D'})
+      .then(fp => fp.get())
+      .then(result => console.log(result.visitorId));
+  }
+</script>
+<script
+  async
+  src="https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs-pro@3/dist/fp.min.js"
+  onload="initFingerprintJS()"
+></script>
 </body>
 </html>
