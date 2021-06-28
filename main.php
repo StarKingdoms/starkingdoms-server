43dd65f73ef8c224b9fc547f920bd94f9af784e6
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-06-28 15:25:48 -0400

    more analytics

--- main.php
+++ main.php
@@ -8,6 +8,7 @@
   <script src="https://cdn.jsdelivr.net/npm/socket.io-client@4.0.1/dist/socket.io.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-L7R4GMFST9"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-L7R4GMFST9');</script>
 </head>
 <body>
   <div id="chatbox">
