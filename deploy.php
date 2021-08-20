d038ef8a4431d42841d6c17e97011e2c66a94209
c0repwn3r <coredoescode@gmail.com>
2021-08-20 13:25:47 -0400

    Remove JS code

--- deploy.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "> repo sync<br>";
-echo "[*] Started goal sync.<br>";
-require("./sync.php");
-echo "Synced server with repository.<br>";
-echo "[*] Finished goal sync.<br>";
-?>
--- get_client_info.php
+++
@@ -1,5 +0,0 @@
-<?php
-// im sorry for using php lolz
-$addr = $_SERVER['REMOTE_ADDR'];
-echo md5($hash);
-?>
