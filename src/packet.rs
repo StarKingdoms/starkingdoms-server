8f1c7ea1b4d1ad8720c52d7d219a39c3f2ffe67d
c0repwn3r <coredoescode@gmail.com>
2021-10-12 17:45:21 +0000

    Add serde derive for packets

--- src/packet.rs
+++ src/packet.rs
@@ -1,33 +1,41 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct PlayerMetadata {
     x_pos: f32,
     y_pos: f32,
     rotation: u16
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct PlayerInput {
     w_key: bool,
     a_key: bool,
     s_key: bool,
     d_key: bool
 }
-    
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct PlayerJoin {
     protocol_version: u8,
     account_id: u32,
     username: String
 }
-    
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct NearbyPlayer {
     x_pos: f32,
     y_pos: f32,
     rotation: u16,
     username: String
 }
-    
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct NearbyPlayers {
     players: Vec<NearbyPlayer>
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ChatMessage {
     message: String,
     sender: String
