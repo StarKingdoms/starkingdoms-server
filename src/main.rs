f4edd1f253bead18520fa7a4dddbb3df24c8b024
GhostlyCoding <pokeball.181818@gmail.com>
2021-10-12 22:35:08 -0500

    Made game loop and started physics portion

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod connection;
 mod game_connection;
 mod packet;
+mod gameloop;
+mod physics;
 
 use async_std::task;
+use gameloop::gameloop;
 
 fn main() {
 	println!("Hello, world!");
-	task::block_on(connection::run("127.0.0.1:8443".to_string()));
+	connection::run("127.0.0.1:8443".to_string());
+    task::block_on(gameloop());
 }
