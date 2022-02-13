a8b5a092b5087de83cb4e3eecff533d566dfde05
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2022-02-13 14:23:02 -0500

    Update core_server_util.js

--- src/core_server_util.js
+++ src/core_server_util.js
@@ -3,9 +3,18 @@ const options = require("./SERVER_OPTIONS.js");
 const express = require('express');
 const app = express();
 const socketio = require('socket.io');
+const { Logger } = require('./logging.js');
+
+logger = new Logger('IOManager');
+logger.info('IOManager ready');
 
 function get_io() {
+	if (fs.existsSync(options.PROD_OVERRIDE)) {
+		logger.info('Production override found, ignoring server_options and disabling development mode');
+		options.DEVELOPMENT_MODE = false;
+	}
 	if (options.DEVELOPMENT_MODE) {
+		logger.info('Creating IOContextDev');
 		const http = require('http').Server(app);
 		// enable insecure server on localhost
 		let io = socketio(http, {
@@ -17,9 +26,10 @@ function get_io() {
 		});
 
 		http.listen(options.PORT);
-
+		logger.info('Created IOContextDev');
 		return io;
 	} else {
+		logger.info('Creating IOContextSSL');
 		const https = require('https');
 		const fs = require('fs');
 		// enable prod secure server on starkingdoms.tk
@@ -38,7 +48,7 @@ function get_io() {
 		});
 
 		server.listen(options.PORT);
-
+		logger.info('Created IOContextSSL');
 		return io;
 	}
 }
